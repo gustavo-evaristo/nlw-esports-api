7b4531368bd1917a48dbd0f1f5e4198062eb6917
Gustavo Evaristo <gug.henri1@gmail.com>
2022-09-16 08:55:32 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
-
-app.use(cors());
 
 app.listen(port, () => console.log(`🚀 server running on ${port}`));
